fc5c7f4e49ddf4440de589ea33cae7646a44bf47
matiaUwU <lazomatias58@gmail.com>
2024-11-03 22:22:54 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let cart = [];
+
+function addToCart(name, price) {
+    cart.push({ name, price });
+    displayCart();
+}
+
+function displayCart() {
+    const cartItems = document.getElementById("cartItems");
+    cartItems.innerHTML = "";
+    cart.forEach((item, index) => {
+        let li = document.createElement("li");
+        li.innerHTML = `${item.name} - $${item.price.toFixed(2)} <button onclick="removeFromCart(${index})">Eliminar</button>`;
+        cartItems.appendChild(li);
+    });
+}
+
+function removeFromCart(index) {
+    cart.splice(index, 1); // Elimina el producto en la posición "index"
+    displayCart(); // Vuelve a mostrar el carrito actualizado
+}
+
+function sendOrder() {
+    const direccion = document.getElementById("direccion").value;
+    if (!direccion) {
+        alert("Por favor, ingresa tu dirección.");
+        return;
+    }
+    let message = "Pedido de Delivery Express:%0A";
+    cart.forEach((item) => {
+        message += `- ${item.name}: $${item.price.toFixed(2)}%0A`;
+    });
+    message += `%0ADirección de entrega: ${direccion}`;
+    const phoneNumber = "591 63699754";
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${message}`;
+    window.open(whatsappURL, "_blank");
+}
+
+
